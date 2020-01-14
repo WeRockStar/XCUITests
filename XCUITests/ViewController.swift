cd9a94c91624ec2d48f4a6253da74915cbf6c114
WeRockStar <kotchaphan.m@gmail.com>
2020-01-14 22:14:57 +0700

    Initial Commit

---
+++ XCUITests/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XCUITests
+//
+//  Created by Kotchaphan Muangsan on 14/1/20.
+//  Copyright © 2020 Kotchaphan Muangsan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
